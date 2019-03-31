6463f66d2156f9f1c33ebc74bfcbc6e2aec6bec5
Anthony DiRusso <ossurid@gmail.com>
2019-03-31 10:12:04 -0400

    creates db info

--- schema.sql
+++ schema.sql
@@ -1,1 +1,7 @@
 --schema.sql--
+create table burrito (
+    id serial primary key,
+    name varchar(200),
+    flavor varchar(200),
+    heat varchar(200)
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,9 @@
 --seed.sql--
+insert into burrito
+    (name, flavor, heat)
+values
+    ('Beth', 'Vegan', 'lava'),
+    ('Bob', 'Vegitarian', 'mild'),
+    ('Betty', 'Bacon', 'hot'),
+    ('Bill', 'Carne', 'volcano')
+;
